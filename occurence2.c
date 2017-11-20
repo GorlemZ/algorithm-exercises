17d50c561947223ce0da765c83a6a754c78c51e6
GorlemZ <andrealice91@yahoo.it>
2017-11-20 21:13:05 +0100

    Add files via upload
    L’obiettivo dell’esercizio è scrivere un programma che sappia fare alcune semplici operazioni riguardanti le occorrenze
    di parole in un testo. Formalmente, una occorrenza è definita come una coppia (word,n), dove word è una parola di
    lettere minuscole e n un intero positivo che rappresenta il numero di occorrenze di word. Sull’insieme delle occorrenze
    consideriamo l’ordine indotto dall’ordinamento alfabetico sulle parole.
    L’input del programma è dato da un testo t, terminato dalla parola STOP, seguito da una sequenza di parole w1,w2,··· ,wn,
    terminata ancora dalla parola STOP. Si assume che tutte le parole abbiano al massimo WORD caratteri, dove il valore di
    WORD è definito nel programma con un’opportuna macro. La punteggiatura va ignorata e non si deve fare distinzione tra
    lettere maiuscole e minuscole.
    L’output deve essere formato da tre parti:
    • l’elenco, in ordine alfabetico, di tutte le occorrenze del testo;
    • lo stesso elenco, ma nell’ordine inverso;
    • il numero di occorrenze della parola wi, per ogni i da 1 a n.

---
+++ occurence2.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define WORD 15
+#define TEXT "text.txt"
+FILE *f;
+
+struct occurrence
+{
+	char word[WORD];
+	int n;
+};
+
+typedef struct occurrence * Occ ;
+
+struct bit_node
+{
+	Occ item ;
+	struct bit_node *l , *r;
+
+};
+
+typedef struct bit_node * Bit_node ;
+
+
+Bit_node bit_new (Occ ite)
+{
+	Bit_node bn;
+	bn= malloc(sizeof(struct bit_node));
+	bn -> item = ite;
+	bn ->l= NULL;
+	bn ->r= NULL;
+
+	return bn;
+}
+
+Occ occ_new(char * wordread)
+{
+	Occ oc;
+	oc= malloc(sizeof(struct occurrence));
+	strcpy( oc -> word, wordread);
+	oc -> n= 1;
+
+	return oc;
+}
+
+Bit_node search_node(char * string, Bit_node head)
+{
+	Bit_node notfound=NULL;
+	Bit_node bn;
+	bn = head;
+
+	int i=strcmp(bn->item->word, string);
+
+	if(i==0)
+	{
+		return bn;
+	}
+	else if(i<0 && bn->r!=NULL)
+	{
+		bn=search_node(string, bn->r);
+	}
+	else if(i>0 && bn->l != NULL)
+	{
+		bn=search_node(string, bn->l);
+	}
+	else
+		return notfound;
+}
+
+
+Bit_node insert_node(Bit_node toinsert, Bit_node head)//inserimento ricorsivo?
+{
+	Bit_node bnfound;
+	Bit_node bn= head;
+	int i;
+
+	if(bn==NULL)
+	{
+		return toinsert;
+	}
+	else
+	{
+
+		if((bnfound=search_node(toinsert->item->word, bn))!=NULL)
+		{
+				bnfound->item->n++;
+		}
+		else
+		{
+			i=strcmp(bn->item->word, toinsert->item->word);
+			if(i<0)//string comparing<0)
+			{
+				if(bn->r ==NULL)
+				{
+					bn->r=toinsert;
+				}
+				else
+				{
+					insert_node(toinsert, bn->r);
+				}
+			}
+			else if (i>0)
+			{
+				if(bn->l ==NULL)
+				{
+					bn->l=toinsert;
+				}
+				else
+				{
+
+					insert_node(toinsert, bn->l);
+				}
+			}
+		}
+	}
+	return bn;
+}
+
+
+void bist_orderprint (Bit_node p) //void bist_orderprint ( Bit_node p );
+{
+
+	if(p)
+	{
+		bist_orderprint (p -> l);
+		printf("*%s ---> %d\n", p->item->word, p->item->n);
+		bist_orderprint (p -> r);
+	}
+
+}
+
+void bist_invorderprint (Bit_node p) //void bist_orderprint ( Bit_node p );
+{
+
+	if(p)
+	{
+		bist_invorderprint (p -> r);
+		printf("* %s ---> %d\n", p->item->word, p->item->n);
+		bist_invorderprint (p -> l);
+	}
+}
+
+
+void bit_printassummary (Bit_node p, int spaces)
+{
+	Bit_node q=p;
+	char sp=' ';
+	int i;
+
+
+	if(!q)
+	{
+		for(i=0; i<spaces*4; i++)
+		{
+			printf("%c", sp);
+		}
+		printf("*\n");
+	}
+
+
+	if(q)
+	{
+		for(i=0; i<spaces*4; i++)
+		{
+			printf("%c", sp);
+		}
+
+		printf("* %s ---> %d\n",q->item->word, q->item->n);
+
+		if(q->l||q->r)
+		{
+			bit_printassummary(q->l, spaces+1);
+			bit_printassummary(q->r, spaces+1);
+		}
+	}
+}
+
+Bit_node mainfirst (char *textname, Bit_node rt)
+{
+	Bit_node root=rt;
+	char *stop= "STOP";
+
+	char c;
+
+	//from text to strings
+	int i=0;
+	char str[WORD]="\0";
+	f=fopen(textname,"rt");
+
+	while((c=fgetc(f))!=EOF)
+	{
+		if(c==' '|| c=='\n')
+		{
+				str[i]='\0';
+
+				if(strcmp(str,stop)!=0)
+				{
+					for (i=0; str[i]; i++)
+					{
+						str[i] = tolower((unsigned char)str[i]);
+					}
+
+					i=0;
+					//qui variabile str pronta
+					//	bit_new( occ_new(str)) funziona, controllare insert_node
+
+					root=insert_node ((bit_new(occ_new(str))),root);
+				}
+				else break;
+		}
+		else if((c>64&&c<91) || (c>96&&c<123))
+		{
+			*(str+i)=c;
+			i++;
+		}
+	}
+
+
+	//fclose(f);
+	return root;
+}
+
+void mainsnd (Bit_node root)
+{
+	int i=0;
+	Bit_node bn;
+	char str[WORD]="\0";
+	char c;
+	char *stop= "STOP";
+
+	while((c=fgetc(f))!=EOF)
+	{
+		if(c==' '|| c=='\n')
+		{
+				str[i]='\0';
+
+				if(strcmp(str,stop)!=0)
+				{
+					for (i=0; str[i]; i++)
+					{
+						str[i] = tolower((unsigned char)str[i]);
+					}
+
+					i=0;
+					//qui variabile str pronta
+					bn=search_node(str,root);
+					if(bn==NULL)
+					{
+						printf("*%s---> 0\n", str);
+					}
+					else
+						printf("*%s---> %d\n", bn->item->word, bn->item->n) ;
+				}
+				else break;
+		}
+		else if((c>64&&c<91) || (c>96&&c<123))
+		{
+			*(str+i)=c;
+			i++;
+		}
+	}
+}
+
+
+
+int main()
+{
+
+	Bit_node root=NULL;
+
+
+	root=mainfirst(TEXT, root);
+
+	printf("\nOCCORRENZE DI PAROLE NEL TESTO IN ORDINE ALFABETICO:\n");
+	printf("****************************************************\n");
+
+	bist_orderprint (root);
+
+	printf("\nOCCORRENZE DI PAROLE NEL TESTO IN ORDINE INVERSO:\n");
+	printf("*************************************************\n");
+
+	bist_invorderprint (root);
+
+	printf("\nRICERCA DI PAROLE:\n");
+	printf("******************\n");
+
+	mainsnd(root);
+
+	fclose(f);
+
+}
